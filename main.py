8c401b40fdca4c57d3716fd8639c5c87828d9215
Ghostfac3 <107084646+Grausamkeitkkj@users.noreply.github.com>
2022-10-20 22:29:54 +0000

    adicionei print

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 #Primeiro projeto em Python!
 
 print("Hello World")
+
+nome = "Diogo Schultz"
+idade = 19
+
+print(nome)
+print(idade)
+
+print("Meu nome é:"+nome)
+print("Minha idade é:"+str(idade))
+print("Minha idade é:{}".format(idade))
+
+
+
