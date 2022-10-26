fd96d1ac68130448325ae53a95f517c49fe14057
Ghostfac3 <107084646+Grausamkeitkkj@users.noreply.github.com>
2022-10-26 23:15:06 +0000

    Adicionei input

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 #Primeiro projeto em Python!
 
-print("Hello World")
+#comandos de saída e variáveis
+#print("Hello World")
 
-nome = "Diogo Schultz"
-idade = 19
+#nome = "Diogo Schultz"
+#idade = 19
 
-print(nome)
-print(idade)
+#print(nome)
+#print(idade)
 
-print("Meu nome é:"+nome)
-print("Minha idade é:"+str(idade))
-print("Minha idade é:{}".format(idade))
+#print("Meu nome é:"+nome)
+#print("Minha idade é:"+str(idade)+" anos")
+#print(f"Meu nome é {nome}")
+#print("Minha idade é:{}".format(idade))
+#print("Meu nome é:{} e minha idade é:{}".format(nome, idade))
 
+#comando de input
 
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+
+print("Seu nome é:{}".format(nome))
+print("Sua idade é:{}".format(idade))
+
+dobro = idade * 2 
+
+print("O dobro da sua idade é: {}".format(dobro))
 
