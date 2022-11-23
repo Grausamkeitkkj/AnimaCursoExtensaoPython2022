34abc1c9b0b02c93e9c84d2c678958edf7e173ad
Ghostfac3 <107084646+Grausamkeitkkj@users.noreply.github.com>
2022-11-23 22:01:37 +0000

    Adicionei banco de dados!

--- bancodedados.py
+++ bancodedados.py
@@ -1,13 +1,29 @@
-import sqlite3
+import funcaoconectar as bd
 
-conexao = sqlite3.connect("dc_universe.db")
+conexao, cursor = bd.conectar()
 
-cursor = conexao.cursor()
+nome = input("Digite o nome do herói/vilão\n")
+nome_civil = input("Digite o nome civil do herói/vilão\n")
+tipo_numerico = input("Tecle 1 para herói(na) ou 2 vilão(ã)")
 
-sql = "SELECT pessoa_id, nome, nome_civil, tipo FROM pessoas"
+sql = 'SELECT MAX(pessoa_id)+1 FROM pessoas'
+cursor.execute(sql)
+pessoa_id = cursor.fetchone()[0]
+pessoas = cursor.fetchall()
+
+if tipo_numerico == "1":
+  tipo = "Herói(na)"
+else:
+  tipo_numerico = "vilão(ã)"
+
+sql = f"INSERT INTO pessoas (pessoa_id, nome, nome_civil, tipo) VALUES ({pessoa_id}, '{nome}', '{nome_civil}', '{tipo}')"
+
+for pessoa in pessoas:
+  print(f"Nome: {pessoa[1]} ({pessoa[3]})")
 
 cursor.execute(sql)
+conexao.commit()
+cursor.close()
 
-pessoas = cursor.fetchall()
-for pessoa in pessoas:
-  print(pessoa[1])
+
+
---
+++ funcaoconectar.py
@@ -0,0 +1,7 @@
+import sqlite3
+
+def conectar():
+  conexao = sqlite3.connect("dc_universe.db")
+  cursor = conexao .cursor()
+  return conexao, cursor
+
